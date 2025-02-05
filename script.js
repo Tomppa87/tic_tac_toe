6f185ec8951cce1b1466ee26d85a91f93231e85c
Thomas Byrne <byrnesy987@gmail.com>
2025-02-05 22:33:07 +0200

    added css and js. Testing functions

---
+++ script.js
@@ -0,0 +1,10 @@
+function createBoard (boardSideGrids) {
+    let grid = new Array(boardSideGrids*boardSideGrids);
+    return {grid}
+}
+
+const three_sided_grid = createBoard(3);
+
+function createPlayer (name) {
+    
+}
