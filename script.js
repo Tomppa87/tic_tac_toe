ee7366537be2f9b90556d2cb0440617879e2df8e
Thomas Byrne <byrnesy987@gmail.com>
2025-02-06 17:36:10 +0200

    checkValid, checkTurn and playTurn functions

--- script.js
+++ script.js
@@ -1,33 +1,71 @@
+let main = document.querySelector(".main")
+
 let gameBoard;
 
-function createBoard(a) {
-    /*const grid = [];    
-    const row = new Array (a); 
-    for (i=0;i<a;i++) {
-        grid.push(row)
-    }    
-    gameBoard = grid;
-    return gameBoard*/
+function createBoard(a) {   
     var board =  Array.from(Array(a), () => {
         return new Array(a).fill("");
     });
     gameBoard = board
     return board
     }
-
-
-
-
-
 console.log(createBoard(3));
 
 function playerTurn(num1,num2) {
-    gameBoard[num1][num2] = "X";
+    let player1Turn;
+    let valid;
+    function checkValid(num1,num2) {
+        valid = (gameBoard[num1][num2] === "");
+        
+        return valid;
+    }
+        console.log(valid)
+        
+    
+    function checkTurn() {
+        if ((count === 0)||(count % 2 == 0)) {
+            player1Turn = true;
+        } else {
+            player1Turn = false
+        };
+        return player1Turn;
+    }
+    function playTurn() {
+        if (valid === true) {
+            if (player1Turn) {
+                gameBoard[num1][num2] = "X";
+                count +=1;
+            }   else {
+                gameBoard[num1][num2] = "O";
+                count +=1;
+            }
+        } else {
+            console.log("Error")
+            
+        }
+    }
+    checkValid(num1,num2);
+    checkTurn();
+    playTurn();
+    
+    
+        
+     
+    
+    
+    
+        
+    
+    
     console.log(gameBoard);
 }
+let count = 0;
 
 function createPlayer (name) {
     return {name}
 }
 
 const player1 = createPlayer("Thomas");
+const player2 = createPlayer("Helena");
+
+
