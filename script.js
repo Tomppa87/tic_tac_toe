9e265aa90d510c1243098f10f69dc39fec5dd954
Thomas Byrne <byrnesy987@gmail.com>
2025-02-06 07:49:51 +0200

    createboard function and early version of turn

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
-const board = (function () {
-    const grid = (a) => new Array(a*a);
-    return {grid};
-})();
+let gameBoard;
 
-console.log(board.grid(3))
+function createBoard(a) {
+    /*const grid = [];    
+    const row = new Array (a); 
+    for (i=0;i<a;i++) {
+        grid.push(row)
+    }    
+    gameBoard = grid;
+    return gameBoard*/
+    var board =  Array.from(Array(a), () => {
+        return new Array(a).fill("");
+    });
+    gameBoard = board
+    return board
+    }
+
+
+
+
+
+console.log(createBoard(3));
+
+function playerTurn(num1,num2) {
+    gameBoard[num1][num2] = "X";
+    console.log(gameBoard);
+}
 
 function createPlayer (name) {
     return {name}
 }
+
+const player1 = createPlayer("Thomas");
